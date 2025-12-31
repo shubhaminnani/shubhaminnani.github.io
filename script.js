439a6de49351fb90dcbd80137d8defa4fe24b5bc
Shubham Innani <shubham.innani@gmail.com>
2025-12-31 16:58:25 -0500

    updates

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,59 +12,31 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Deep Learning Enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: ["AI Researcher", "Research Analyst: Computational Pathology", "Machine Learning Engineer"],
+        typeSpeed: 60,
+        backSpeed: 40,
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["Deep Learning Enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
+    var typed2 = new Typed(".typing-2", {
+        strings: ["AI & Digital Pathology", "Clinical AI Tool Development", "Whole-Slide Image Analysis"],
+        typeSpeed: 60,
+        backSpeed: 40,
         loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
     });
 });
