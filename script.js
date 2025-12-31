f3576650657f0facbcfd6a2c9668b99b0b99440c
Shubham Innani <shubham.innani@gmail.com>
2025-12-31 17:18:46 -0500

    updraft

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar logic
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
-        }
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
         }
     });
 
